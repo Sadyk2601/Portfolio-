3c85c65a9ae143c9fb73b66e6c6dbfe0ebe4fea5
Sadyk2601 <you@example.com>
2025-03-03 19:42:32 +0500

    update homepage

--- middlewares/app.js
+++ middlewares/app.js
@@ -3,10 +3,10 @@ let renderPage = require("../routes/render.route");
 let app = express();
 let cors = require("cors");
 app.use(express.json());
-app.set("views", "./public/johndoe/public_html/views");
+app.set("views", "./public/johndoe");
 app.set("view engine", "ejs");
 app.use(cors("*"));
-app.use(express.static("./public/johndoe/public_html"));
+app.use(express.static("./public/johndoe"));
 app.use("/", renderPage);
 
 app.use("*", (req, res) => {
